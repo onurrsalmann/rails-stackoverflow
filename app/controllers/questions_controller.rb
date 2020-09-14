13a2d64aeff174d27951300e7b3cb5a53944e1ad
onurrsalmann <salmannonur@gmail.com>
2020-09-14 15:29:27 +0300

    Add devise user model

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -56,7 +56,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
     respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Question was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
